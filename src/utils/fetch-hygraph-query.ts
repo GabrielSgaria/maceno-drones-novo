5706b34375216a3d9d5743cdf7733ae8966548ae
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 17:05:58 -0300

    teste

--- src/utils/fetch-hygraph-query.ts
+++ src/utils/fetch-hygraph-query.ts
@@ -3,7 +3,8 @@ export const fetchHygraphQuery = async (query: string) => {
         const response = await fetch(process.env.HYGRAPH_URL!, {
             method: 'POST',
             headers: {
-                'content-Type': 'application/json',
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
                 Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
             },
             body: JSON.stringify({ query }),
