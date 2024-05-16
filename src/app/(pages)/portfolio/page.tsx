ac62cbe59a35d51c1ba3a9afb9a843cf63d9abf6
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 14:56:19 -0300

    feat: type 2

--- src/app/(pages)/portfolio/page.tsx
+++ src/app/(pages)/portfolio/page.tsx
@@ -3,15 +3,20 @@ import { getAllProjects } from "@/utils/actions";
 import Image from "next/image";
 
 export default async function Portfolio() {
-  const { portfolios } = await getAllProjects();
+  try {
+    const { portfolios } = await getAllProjects();
 
-  return (
-    <div>
-      {portfolios.length > 0 ? (
-        <ProjectList portfolios={portfolios} />
-      ) : (
-        <div>Não foram encontrados projetos.</div>
-      )}
-    </div>
-  );
+    return (
+      <div>
+        {portfolios.length > 0 ? (
+          <ProjectList portfolios={portfolios} />
+        ) : (
+          <div>Não foram encontrados projetos.</div>
+        )}
+      </div>
+    );
+  } catch (error) {
+    console.error("Erro ao carregar portfolios:", error);
+    return <div>Erro ao carregar portfolios. Por favor, tente novamente mais tarde.</div>;
+  }
 }
