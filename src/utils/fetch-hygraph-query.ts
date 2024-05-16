7404f13a0a05a1b0765e48ea20de4d8995db5bac
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 16:47:08 -0300

    teste try

--- src/utils/fetch-hygraph-query.ts
+++ src/utils/fetch-hygraph-query.ts
@@ -1,16 +1,17 @@
 export const fetchHygraphQuery = async (query: string) => {
-    const response = await fetch(process.env.HYGRAPH_URL!, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
-        },
-        body: JSON.stringify({ query }),
-        next: {
-            revalidate: 60 * 60 * 24 //24 hours
-        }
-    })
     try {
+        const response = await fetch(process.env.HYGRAPH_URL!, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
+            },
+            body: JSON.stringify({ query }),
+            next: {
+                revalidate: 60 * 60 * 24 //24 hours
+            }
+        })
+
         const { data } = await response.json()
         return data
     } catch (err) {
