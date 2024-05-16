66344ced89d455eee75e960a38c639ef44567904
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 15:17:10 -0300

    fetch

--- src/app/(pages)/portfolio/page.tsx
+++ src/app/(pages)/portfolio/page.tsx
@@ -2,21 +2,14 @@ import ProjectList from "@/components/project-list";
 import { getAllProjects } from "@/utils/actions";
 import Image from "next/image";
 
-export default async function Portfolio() {
-  try {
-    const { portfolios } = await getAllProjects();
 
+export default async function Portfolio() {
+    const { portfolios } = await getAllProjects();
+    // console.log(portfolios)
     return (
-      <div>
-        {portfolios.length > 0 ? (
-          <ProjectList portfolios={portfolios} />
-        ) : (
-          <div>Não foram encontrados projetos.</div>
-        )}
-      </div>
-    );
-  } catch (error) {
-    console.error("Erro ao carregar portfolios:", error);
-    return <div>Erro ao carregar portfolios. Por favor, tente novamente mais tarde.</div>;
-  }
+        <div>
+            <ProjectList portfolios={portfolios} />
+        </div>
+
+    )
 }
--- src/utils/fetch-hygraph-query.ts
+++ src/utils/fetch-hygraph-query.ts
@@ -1,15 +1,23 @@
-export const fetchHygraphQuery = async (query: string) => {
+export const fetchHygraphQuery = async <T>(
+    query: string,
+    revalidate?: number,
+  ): Promise<T> => {
     const response = await fetch(process.env.HYGRAPH_URL!, {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json',
-            Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`
-        },
-        body: JSON.stringify({query}),
-        next: {
-            revalidate: 60 * 60 * 24 //24 hours
-        }
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+      },
+      next: {
+        revalidate,
+      },
+      body: JSON.stringify({
+        query,
+      }),
     })
-    const {data} = await response.json()
+  
+    const { data } = await response.json()
+  
     return data
-}
+  }
