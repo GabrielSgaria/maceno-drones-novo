5e572569763edd16284e0b5b8baff4261c1e1a90
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 11:44:21 -0300

    feat: video projects

--- src/components/project-details.tsx
+++ src/components/project-details.tsx
@@ -16,9 +16,11 @@ type ProjectDetailsProps = {
           url: string | undefined
         }
       ]
-      videoDesktop: {
-        url: string | undefined
-      }
+      videoDesktop: [
+        {
+          url: string | undefined
+        }
+      ]
 
     }
   }
@@ -27,17 +29,35 @@ type ProjectDetailsProps = {
 export async function ProjectDetails({ projectSlug }: ProjectDetailsProps) {
   let detailsProject = projectSlug.portfolio
   return (
-    <div>
-      <div className="container mx-auto py-10">
+    <div className="container mx-auto py-10">
+      <div className="flex flex-col items-center justify-center gap-5">
         <h1 className="text-3xl font-bold uppercase">{detailsProject.nomeDoProjeto}</h1>
+        <p className="text-lg w-full max-w-[900px] text-justify">{detailsProject.descricao}</p>
       </div>
-      <div className="py-16 container mx-auto ">
-        {detailsProject.fotos.map((fotos, i) => (
-          <div key={i}>
-            <Image width={900} height={900} src={`${fotos.url}`} alt={detailsProject.nomeDoProjeto} />
+
+      <div className="flex items-center flex-col mt-20">
+        <h1 className="text-3xl font-bold text-start w-full uppercase">Fotos Profissionais</h1>
+        <div className="grid grid-flow-row grid-cols-2 gap-2 mt-10">
+          {detailsProject.fotos.map((fotos, i) => (
+            <div key={i}>
+              <Image width={900} height={900} src={`${fotos.url}`} alt={detailsProject.nomeDoProjeto} className="rounded-xl" />
+            </div>
+          ))}
+        </div>
+      </div>
+      {detailsProject.videoDesktop.length > 0 && (
+        <div className="flex items-center flex-col mt-20">
+          <h1 className="text-3xl font-bold text-start w-full uppercase">Videos Profissionais</h1>
+          <div className="flex flex-col items-center">
+
+            {detailsProject.videoDesktop.map((videoDesktop, i) => (
+              <div key={i} className="w-max flex flex-col items-center justify-center mb-10 bg-zinc-200 rounded-xl shadow-xl border border-zinc-300/80 p-3">
+                <video src={`${videoDesktop.url}`} controls muted preload="true" className="h-full max-h-[600px] min-w-[200px] w-min rounded-lg" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
