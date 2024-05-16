3d2aec36db4a2d075a84aa6a90e4481c3a6cfa73
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-16 16:45:11 -0300

    teste portfolios

--- src/types/portfolio-info.ts
+++ src/types/portfolio-info.ts
@@ -1,71 +1,69 @@
 export type PortfolioInfo =
-    {
-        id: string,
-        descricao: string,
-        nomeDoProjeto: string,
-        slugProjeto: string,
-        fotoCapa: {
-            url: string
-        },
-        fotos: [
-            {
-                url: string | undefined
-            }
-        ]
-        videoDesktop: {
-            url: string | undefined
-        }
-
+  {
+    id: string,
+    descricao: string,
+    nomeDoProjeto: string,
+    slugProjeto: string,
+    fotoCapa: {
+      url: string
+    },
+    fotos: [
+      {
+        url: string | undefined
+      }
+    ]
+    videoDesktop: {
+      url: string | undefined
     }
+  }
 
 export type PortfolioContentData = {
-    portfolios?: PortfolioInfo[],
+  portfolios?: PortfolioInfo[],
 }
 
 export type PageProjectsData = {
-    portfolio: {
-        id: string,
-        descricao: string,
-        nomeDoProjeto: string,
-        slugProjeto: string,
-        fotoCapa: {
-            url: string
-        },
-        fotos: [
-            {
-                url: string | null
-            }
-        ]
-        videoDesktop: [
-            {
-                url: string | null
-            }
-        ]
-
-    }
+  portfolio: {
+    id: string,
+    descricao: string,
+    nomeDoProjeto: string,
+    slugProjeto: string,
+    fotoCapa: {
+      url: string
+    },
+    fotos: [
+      {
+        url: string | null
+      }
+    ]
+    videoDesktop: [
+      {
+        url: string | null
+      }
+    ]
+  }
 }
 
-export type  ProjectDetailsProps = {
-    projectSlug: {
-      portfolio: {
-        id: string,
-        descricao: string,
-        nomeDoProjeto: string,
-        slugProjeto: string,
-        fotoCapa: {
-          url: string
-        },
-        fotos: [
-          {
-            url: string | null
-          }
-        ]
-        videoDesktop: [
-          {
-            url: string | null
-          }
-        ]
-  
-      }
+export type ProjectDetailsProps = {
+  projectSlug: {
+    portfolio: {
+      id: string,
+      descricao: string,
+      nomeDoProjeto: string,
+      slugProjeto: string,
+      fotoCapa: {
+        url: string
+      },
+      fotos: [
+        {
+          url: string | null
+        }
+      ]
+      videoDesktop: [
+        {
+          url: string | null
+        }
+      ]
+
     }
   }
+}
