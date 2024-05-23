add1a60170891328465e06fd892d54e1586932e6
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-05-23 20:34:18 -0300

    fix: logos

--- src/lib/images-clients.tsx
+++ src/lib/images-clients.tsx
@@ -1,11 +1,23 @@
-import logoRock from '../../public/image/clients/1.png' 
-import sansFelix from '../../public/image/clients/2.png' 
+import logoRock from '../../public/image/clients/1.png'
+import sansFelix from '../../public/image/clients/2.png'
 import logoF from '../../public/image/clients/3.png'
-import logoFirst from '../../public/image/clients/4.png' 
-import logoDoceEnergia from '../../public/image/clients/5.png' 
-import logoQuest from '../../public/image/clients/6.png' 
-import logoVlImoveis from '../../public/image/clients/7.png' 
-import logoGueesHouse from '../../public/image/clients/8.png' 
+import logoFirst from '../../public/image/clients/4.png'
+import logoDoceEnergia from '../../public/image/clients/5.png'
+import logoQuest from '../../public/image/clients/6.png'
+import logoVlImoveis from '../../public/image/clients/7.png'
+import logoGueesHouse from '../../public/image/clients/8.png'
+import logoSlep from '../../public/image/clients/9.png'
+import logoTrue from '../../public/image/clients/10.png'
+import logoCapoeira from '../../public/image/clients/11.jpg'
+import logoKing from '../../public/image/clients/12.png'
+import logoPcDrift from '../../public/image/clients/13.jpg'
+import logoCidadeDaLuta from '../../public/image/clients/14.jpg'
+import logoBDN from '../../public/image/clients/15.jpg'
+import logoResSolELua from '../../public/image/clients/16.png'
+import logoSimoes from '../../public/image/clients/17.jpg'
+import logoMgmMarcenaria from '../../public/image/clients/18.png'
+import logoALM from '../../public/image/clients/19.jpg'
+
 
 
 
@@ -18,5 +30,15 @@ export const imagesLogo = [
     { src: logoQuest, alt: 'Logo Quest Construtora' },
     { src: logoVlImoveis, alt: 'Logo VL Imóveis' },
     { src: logoGueesHouse, alt: 'Logo Guess House iphones' },
-
+    { src: logoSlep, alt: 'Logo Slep' },
+    { src: logoTrue, alt: 'Logo True' },
+    { src: logoCapoeira, alt: 'Logo Centro Cultural Grupo Guerreiros do Brasil - Capoeira' },
+    { src: logoKing, alt: 'Logo Barbearia King' },
+    { src: logoPcDrift, alt: 'Logo Pc 1.27 Drift' },
+    { src: logoCidadeDaLuta, alt: 'Logo Cidade da Luta' },
+    { src: logoBDN, alt: 'Logo B D N' },
+    { src: logoResSolELua, alt: 'Logo Restaurante Sol e Lua' },
+    { src: logoSimoes, alt: 'Logo Simões Consoltoria Imobiliaria' },
+    { src: logoMgmMarcenaria, alt: 'Logo MGM Marcenaria' },
+    { src: logoALM, alt: 'Logo ALM auto park' },
 ]
