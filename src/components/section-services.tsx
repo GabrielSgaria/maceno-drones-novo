568319c7c04b13a4fe405721103cc1696b195fc0
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-07-04 09:12:01 -0300

    feat: clientes e  card novo

--- src/components/section-services.tsx
+++ src/components/section-services.tsx
@@ -4,28 +4,28 @@ export function SectionServices() {
     return (
         <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 mx-auto container gap-10 sm:gap-20 px-4 sm:px-0">
             <CardServices
-                imageCardServices="/image/fotos/DJI_0548.JPG"
+                imageCardServices="https://media.graphassets.com/uWhffDxgSdmKz738yXU2"
                 altCardServices="Institucional Image"
                 textCardServices="Vídeos Institucionais"
                 descCardServices="Eleve o visual da sua marca com vídeos institucionais de alta qualidade. capturo valores, missão e serviços de forma profissional, usando filmagem avançada e drones para destacar sua marca."
 
             />
             <CardServices
-                imageCardServices="/image/fotos/dji_fly_20231122_084012_150_1700653219772_photo_optimized.jpg"
-                altCardServices="Vídeos Imobiliários"
+                imageCardServices="https://media.graphassets.com/TJVkxHKyS6yVOTlk9fEa"
+                altCardServices="Gravação de Set"
                 textCardServices="Vídeos Imobiliários"
-                descCardServices="Destaque imóveis com vídeos de alta qualidade. Registro cada detalhe, oferecendo uma experiência envolvente que atrai compradores em potencial."
+                descCardServices="Nada melhor do que um vídeo set para mostrar o seu trabalho em grande estilo e abrir ainda mais as portas do mercado. Grave o seu vídeo set em Full HD para você postar em sites como YouTube, Twitch e Vimeo, com áudio masterizado."
 
             />
             <CardServices
-                imageCardServices="https://media.graphassets.com/W0Xs3hX9RXO7VO7vdkod"
+                imageCardServices="https://media.graphassets.com/cgsgFmJVQtydDlQYeB7s"
                 altCardServices="Eventos"
                 textCardServices="Eventos"
                 descCardServices="Capture a emoção do evento com um vídeo profissional. Especializado em cobrir eventos de lutas, drift de carros, shows de manobras radicais. Com técnicas avançadas de filmagem e drones, garanto um vídeo impactante e memorável."
 
             />
             <CardServices
-                imageCardServices="https://media.graphassets.com/SpjkHy1R5urLmKVy6uNV"
+                imageCardServices="https://media.graphassets.com/0K7eMTOQ0O1qm4cOVowA"
                 altCardServices="Criativo para anuncios"
                 textCardServices="Criativo para anuncios"
                 descCardServices="Maximize o impacto da sua campanha com vídeos dinâmicos. Destaco ofertas e produtos de forma atraente, capturando a atenção do público-alvo e impulsionando suas vendas com produções inovadoras e profissionais."
--- src/lib/images-clients.tsx
+++ src/lib/images-clients.tsx
@@ -3,8 +3,8 @@ import sansFelix from '../../public/image/clients/2.png'
 import logoF from '../../public/image/clients/3.png'
 import logoFirst from '../../public/image/clients/4.png'
 import logoDoceEnergia from '../../public/image/clients/5.png'
-import logoQuest from '../../public/image/clients/6.png'
-import logoVlImoveis from '../../public/image/clients/7.png'
+import logoHavana from '../../public/image/clients/6.png'
+import logoM2 from '../../public/image/clients/7.png'
 import logoGueesHouse from '../../public/image/clients/8.png'
 import logoSlep from '../../public/image/clients/9.png'
 import logoTrue from '../../public/image/clients/10.png'
@@ -14,9 +14,16 @@ import logoPcDrift from '../../public/image/clients/13.jpg'
 import logoCidadeDaLuta from '../../public/image/clients/14.jpg'
 import logoBDN from '../../public/image/clients/arq15.jpg'
 import logoResSolELua from '../../public/image/clients/16.png'
-import logoSimoes from '../../public/image/clients/17.jpg'
+import logoSargentoAlmeida from '../../public/image/clients/17.jpeg'
 import logoMgmMarcenaria from '../../public/image/clients/18.png'
 import logoALM from '../../public/image/clients/arq19.jpg'
+import vetorelo from '../../public/image/clients/20.jpg'
+import progressistas from '../../public/image/clients/21.png'
+import patampinhas from '../../public/image/clients/22.png'
+import ranch from '../../public/image/clients/23.png'
+import sotenis from '../../public/image/clients/24.png'
+import pmPr from '../../public/image/clients/25.png'
+
 
 
 export const imagesLogo = [
@@ -25,8 +32,8 @@ export const imagesLogo = [
     { src: logoF, alt: 'Logo F' },
     { src: logoFirst, alt: 'Logo First Class' },
     { src: logoDoceEnergia, alt: 'Logo Doce Energia' },
-    { src: logoQuest, alt: 'Logo Quest Construtora' },
-    { src: logoVlImoveis, alt: 'Logo VL Imóveis' },
+    { src: logoHavana, alt: 'Logo Havana' },
+    { src: logoM2, alt: 'Logo M2' },
     { src: logoGueesHouse, alt: 'Logo Guess House iphones' },
     { src: logoSlep, alt: 'Logo Slep' },
     { src: logoTrue, alt: 'Logo True' },
@@ -36,7 +43,13 @@ export const imagesLogo = [
     { src: logoCidadeDaLuta, alt: 'Logo Cidade da Luta' },
     { src: logoBDN, alt: 'Logo B D N' },
     { src: logoResSolELua, alt: 'Logo Restaurante Sol e Lua' },
-    { src: logoSimoes, alt: 'Logo Simões Consoltoria Imobiliaria' },
+    { src: logoSargentoAlmeida, alt: 'Logo Sargento Almeida' },
     { src: logoMgmMarcenaria, alt: 'Logo MGM Marcenaria' },
-    { src: logoALM, alt: 'Logo ALM auto park' },
+    { src: logoALM, alt: 'Logo ALM' },
+    { src: vetorelo, alt: 'Logo Vetorelo' },
+    { src: progressistas, alt: 'Logo progressistas' },
+    { src: patampinhas, alt: 'Logo patampinhas' },
+    { src: ranch, alt: 'Logo ranch' },
+    { src: sotenis, alt: 'Logo Só Tenis' },
+    { src: pmPr, alt: 'Logo Pm PR' },
 ]
